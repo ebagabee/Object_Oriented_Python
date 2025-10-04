21785726fd57d911c4a355824121587ab4296389
ebagabee <ebagabe.2025@gmail.com>
2025-10-04 14:50:23 -0300

    feat: implement overlaps method in Rectangle class and add README for functionality description

---
+++ Polymorphism/02. Sobreposicao/main.py
@@ -0,0 +1,48 @@
+class Rectangle:
+    def overlaps(self, rect):
+        self_minor_x = self.get_left_x()
+        self_major_x = self.get_right_x()
+
+        self_major_y = self.get_top_y()
+        self_minor_y = self.get_bottom_y()
+
+        rect_minor_x = rect.get_left_x()
+        rect_major_x = rect.get_right_x()
+
+        rect_major_y = rect.get_top_y()
+        rect_minor_y = rect.get_bottom_y()
+
+        if self_minor_x > rect_major_x or self_major_x < rect_minor_x or self_minor_y > rect_major_y or self_major_y < rect_minor_y:
+            return False
+
+        return True
+    # don't touch below this line
+
+    def __init__(self, x1, y1, x2, y2):
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__x2 = x2
+        self.__y2 = y2
+
+    def get_left_x(self):
+        if self.__x1 < self.__x2:
+            return self.__x1
+        return self.__x2
+
+    def get_right_x(self):
+        if self.__x1 > self.__x2:
+            return self.__x1
+        return self.__x2
+
+    def get_top_y(self):
+        if self.__y1 > self.__y2:
+            return self.__y1
+        return self.__y2
+
+    def get_bottom_y(self):
+        if self.__y1 < self.__y2:
+            return self.__y1
+        return self.__y2
+
+    def __repr__(self):
+        return f"Rectangle({self.__x1}, {self.__y1}, {self.__x2}, {self.__y2})"
